295c4f018d167b84cbe4c2e8ebe054ba072fcfc3
vinsdragonis <zrexteam128@gmail.com>
2022-10-08 20:28:36 +0530

    Reworked front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
-import NavBar from './components/NavBar/NavBar';
-// import MainCarousel from './components/MainCarousel/MainCarousel';
-import Header from './components/Header/Header';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-		<NavBar />
-		<Header />
-		{/* <MainCarousel /> */}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
